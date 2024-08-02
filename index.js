32db852adcb9e7bdc2724811add78303de075b7f
ShrawanVD <shra1.deo7714@gmail.com>
2024-08-02 11:34:21 +0530

    cors issue resolved, since localhost was not working

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-
 import express from 'express';
 import cors from 'cors';
 import connectToMongo from './database/db.js';
 import mastersheet from './routes/mastersheet.js';
 import client from './routes/client.js';
-import users  from './routes/user.js';
+import users from './routes/user.js';
 
 connectToMongo();
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
 
-// Available routes    
+// CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:4200', // specify the allowed origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // enable to include cookies in the requests
+    optionsSuccessStatus: 204
+};
+app.use(cors(corsOptions));
+
+// Available routes
 app.get('/', (req, res) => {
-    res.send('crm api are working')
-})
+    res.send('crm api are working');
+});
 
 app.use('/api/master', mastersheet);
 app.use('/api/client', client);
-app.use('/api',users)
+app.use('/api', users);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
